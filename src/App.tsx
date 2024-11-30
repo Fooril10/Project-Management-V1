4b53d7080ba9235b60774fb29bd0665e0b5e7661
Fooril10 <Diane@jcemedia.com>
2024-11-30 23:23:58 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Navigation } from './components/layout/Navigation';
+import { CalendarView } from './pages/CalendarView';
+import { ProjectsView } from './pages/ProjectsView';
+import { AllTasksView } from './pages/AllTasksView';
+import { OptimizationNotesView } from './pages/OptimizationNotesView';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navigation />
+        <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <Routes>
+            <Route path="/calendar" element={<CalendarView />} />
+            <Route path="/projects" element={<ProjectsView />} />
+            <Route path="/tasks" element={<AllTasksView />} />
+            <Route path="/notes" element={<OptimizationNotesView />} />
+            <Route path="/" element={<Navigate to="/calendar" replace />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
